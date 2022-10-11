f86b0184cc8dc93a557011a11753155ac8d96330
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-10-11 22:33:38 +0300

    fixed tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import React from "react";
 
-export default function App() {
+export const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-red-200">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
