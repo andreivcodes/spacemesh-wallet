5dd1abe425c3538df27723755df6b85534d58c34
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-02-15 11:27:57 +0200

    RUNS

--- App.tsx
+++ App.tsx
@@ -1,18 +1,69 @@
+import "react-native-get-random-values";
+import "node-libs-react-native/globals.js";
+global.Buffer = require("buffer").Buffer;
+
 import "./styles.css";
 
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+
+import { grpc } from "@improbable-eng/grpc-web";
+import { NodeHttpTransport } from "@improbable-eng/grpc-web-node-http-transport";
+import { MeshServicePromiseClient } from "./src/proto/build/spacemesh/v1/mesh_grpc_web_pb";
+
+grpc.setDefaultTransport(NodeHttpTransport());
 
 export default function App() {
+  const [network, setNetwork] = useState("Loading...");
+  const [currentLayer, setCurrentLayer] = useState("Loading...");
+
+  const promiseClient = new MeshServicePromiseClient(
+    `http://127.0.0.1:8080`,
+    null,
+    null
+  );
+
+  const callGrpcServicePromise = async () => {
+    const result = await promiseClient.currentLayer(
+      new proto.spacemesh.v1.CurrentLayerRequest(),
+      {}
+    );
+    setCurrentLayer(JSON.stringify(result.toObject()));
+  };
+
+  useEffect(() => {
+    async function work() {
+      const network = await getNetwork();
+      setNetwork(network);
+    }
+    work();
+  }, []);
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-green-700 text-lg font-bold mt-2">
         Hello, world!
       </Text>
+      <Text className="text-green-700 text-sm font-bold mt-2">{network}</Text>
+      <Button
+        title="GRPC Request"
+        onPress={() => callGrpcServicePromise()}
+      ></Button>
       <Text className="text-green-700 text-sm font-bold mt-2">
-        This will probably be a very cool project
+        Current layer: {currentLayer}
       </Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+async function getNetwork() {
+  const res = await fetch("https://discover.spacemesh.io/networks.json")
+    .then((response) => response.json())
+    .then((res: any) => {
+      return res[0]["grpcAPI"].slice(0, -1).substring(8);
+    });
+
+  return res;
 }
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,25 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push("css");
-
-module.exports = config;
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    // extraNodeModules: {
+    //   crypto: require.resolve("crypto-browserify"),
+    //   url: require.resolve("url"),
+    //   fs: require.resolve("expo-file-system"),
+    //   http: require.resolve("stream-http"),
+    //   https: require.resolve("https-browserify"),
+    //   net: require.resolve("react-native-tcp"),
+    //   os: require.resolve("os-browserify/browser.js"),
+    //   path: require.resolve("path-browserify"),
+    //   stream: require.resolve("readable-stream"),
+    //   vm: require.resolve("vm-browserify"),
+    //   buffer: require.resolve("buffer/"),
+    // },
+    extraNodeModules: require("node-libs-react-native"),
+    assetExts: [...defaultConfig.resolver.assetExts, "css"],
+  },
+};
